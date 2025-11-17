a7217c84a4f16c4b329a2550b9ad5f480a24f8d4
Wht Mst <ven000mus@gmail.com>
2025-11-18 00:01:02 +0400

    Update T-Vamp.lua

--- T-Vamp.lua
+++ T-Vamp.lua
@@ -7,27 +7,59 @@ T_Vamp.vamp_stats = {}
 T_Vamp.tooltip = CreateFrame("GameTooltip", "TVampToolTip", nil, "GameTooltipTemplate")
 T_Vamp.tooltip:SetOwner(WorldFrame, "ANCHOR_NONE")
 
+-- Регистрируем команду
+SLASH_TVAMP1 = "/tvamp"
+SlashCmdList["TVAMP"] = function(msg)
+    T_Vamp:ShowVampStats()
+end
+
 T_Vamp:RegisterEvent("UNIT_HEALTH")
 T_Vamp:RegisterEvent("PLAYER_REGEN_DISABLED")
 T_Vamp:RegisterEvent("PLAYER_REGEN_ENABLED")
 T_Vamp:RegisterEvent("PLAYER_ENTERING_WORLD")
--- T_Vamp:RegisterEvent("UNIT_INVENTORY_CHANGED")
+-- T_Vamp:RegisterEvent("UNIT_INVENTORY_CHANGED") -- Оставляем закомментированным
 T_Vamp:RegisterEvent("UNIT_COMBAT")
 T_Vamp:SetScript("OnEvent", function ()
   T_Vamp[event](T_Vamp,arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,arg9)
 end)
 
+function T_Vamp:ShowVampStats()
+    self:CollectVampStat()
+    
+    local vamp_stats_string = ""
+    if table.getn(self.vamp_stats) > 0 then
+        vamp_stats_string = table.concat(self.vamp_stats, ",") .. " (всего: " .. self.vamp_stat_total .. "%)"
+    else
+        vamp_stats_string = "нет"
+    end
+    
+    DEFAULT_CHAT_FRAME:AddMessage("|cFFFFFF00T-Vamp:|r Статы вампиризма: " .. vamp_stats_string)
+end
+
 function T_Vamp:CollectVampStat()
+  -- Полностью очищаем предыдущие значения
   self.vamp_stat_total = 0
   self.vamp_stats = {}
   
   for slot=1,19 do
+    -- ОЧИЩАЕМ ТУЛТИП перед каждым предметом
+    self.tooltip:ClearLines()
     self.tooltip:SetInventoryItem("player", slot)
+    
     for line=1, self.tooltip:NumLines() do
       local left = getglobal("TVampToolTip" .. "TextLeft" .. line)
       local ltext = left:GetText()
       if ltext then
-        local _,_,vamp = string.find(ltext, "^Equip: (%d+)%% of damage dealt is returned as healing.")
+        -- Только две фразы для поиска
+        local vamp = nil
+        
+        -- Стандартный вариант
+        local _,_,v1 = string.find(ltext, "^Equip: (%d+)%% of damage dealt is returned as healing.")
+        -- Vampirism вариант
+        local _,_,v2 = string.find(ltext, "(%d+)%% Vampirism")
+        
+        vamp = v1 or v2
+        
         if vamp then
           vamp = tonumber(vamp)
           self.vamp_stat_total = self.vamp_stat_total + vamp
@@ -38,7 +70,7 @@ function T_Vamp:CollectVampStat()
   end
 end
 
--- skip combat log parsing for heals by using UNIT_COMBAT
+-- Пропускать анализ лога боя для исцелений, используя UNIT_COMBAT
 function T_Vamp:UNIT_COMBAT(target,action,atype,amount,unknown)
   if not self.in_combat then return end
   if target ~= "player" then return end
@@ -52,11 +84,6 @@ function T_Vamp:UNIT_COMBAT(target,action,atype,amount,unknown)
   end
 end
 
-function T_Vamp:UNIT_INVENTORY_CHANGED(who)
-  if who ~= "player" then return end
-  self:CollectVampStat()
-end
-
 function T_Vamp:PLAYER_REGEN_DISABLED()
   self.in_combat = GetTime()
   self.running_total = 0
@@ -68,13 +95,14 @@ function T_Vamp:PLAYER_ENTERING_WORLD()
   self:CollectVampStat()
 end
 
+-- Сообщение, отправляемое в аддон MikScrollingBattleText (MSBT)
 function T_Vamp:PLAYER_REGEN_ENABLED()
   if self.running_total > 0 then
-    if MikCEH then MikCEH.ParseForIncomingSpellHeals(format("Вы получаете %d здоровья от вампиризма.", self.running_total)) end
+    if MikCEH then MikCEH.ParseForIncomingSpellHeals(format("You gain %d health from Vampirism.", self.running_total)) end
     
     -- Создаем строку со статами вампиризма
     local vamp_stats_string = ""
-    if #self.vamp_stats > 0 then
+    if table.getn(self.vamp_stats) > 0 then
       vamp_stats_string = table.concat(self.vamp_stats, ",") .. " (всего: " .. self.vamp_stat_total .. "%)"
     else
       vamp_stats_string = "нет"
@@ -94,8 +122,9 @@ function T_Vamp:UNIT_HEALTH(unit)
   local diff = now_health - self.last_health
   if diff > 0 then
     self.running_total = self.running_total + diff
+    -- Сообщение, отправляемое в аддон MikScrollingBattleText (MSBT)
     if MikCEH then
-      MikCEH.ParseForIncomingSpellHeals(format("Вы получаете %d здоровья от вампиризма.", diff))
+      MikCEH.ParseForIncomingSpellHeals(format("You gain %d health from Vampirism.", diff))
     end
   end
   self.last_health = now_health
